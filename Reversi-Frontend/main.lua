b2ce87b1285f23e3b4a008a13fdc00a0d0e292bd
nesdown <glushko.bohdan@gmail.com>
2020-10-17 14:56:09 +0300

    Update the values inside

--- Reversi-Frontend/main.lua
+++ Reversi-Frontend/main.lua
@@ -10,6 +10,15 @@ function love.load()
     require "restart"
     exit.load(20, 680)
     restart.load(360, 680)
+
+
+    require "deck"
+    -- Debug elements
+    debug_x = 0
+    debug_y = 0
+
+    count_x = 0
+    count_y = 0
 end
 
 function love.update(dt)
@@ -20,6 +29,13 @@ end
 function love.mousereleased(mx, my)
     exit.mouseaction(mx, my)
     restart.mouseaction(mx, my)
+    
+    -- Debugging part
+    debug_x = mx
+    debug_y = my
+
+    -- Here we are getting the debug data
+    count_x, count_y = deck.mousereleased(mx, my, 1)
 end
 
 function love.draw()
@@ -33,4 +49,8 @@ function love.draw()
     -- Draw other elements
     exit.draw()
     restart.draw()
+
+    -- Debugging Log
+    love.graphics.print("X: " .. debug_x .. " Y: " .. debug_y .. " Horizontal Block Number: " .. count_x .. " Vertical Block Number: " .. count_y, 10, 10)
+    love.graphics.print(math.floor(3/2), 10, 50)
 end
